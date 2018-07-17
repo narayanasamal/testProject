971612040487b18d4e510bd909366bce913fc37d
SatyaNarayana <satya.narayana.samal@ufinance.com>
2018-07-17 14:19:25 +0530

    frist_commite

--- runIN_Git/src/runIN_Git/Git_demo.java
+++ runIN_Git/src/runIN_Git/Git_demo.java
@@ -1,12 +1,13 @@
 package runIN_Git;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Git_demo {
 
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\chrome\\chromedriver.exe");
+	public static void main(String[] args) throws Exception {
+		  System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\chrome\\chromedriver.exe");
 		   // ChromeOptions options=new ChromeOptions();
 		    // options.addArguments("window-size=1300,900");
 		     //DesiredCapabilities capabilites=DesiredCapabilities.chrome();
@@ -16,7 +17,26 @@ public class Git_demo {
 		     //WebDriver driver=new ChromeDriver();
 		     
 		     driver.manage().window().maximize();
+		     
 		     driver.get("https://paasqa.azurewebsites.net");
+		     
+		     
+		     driver.findElement(By.xpath("//input[@type='email']")).sendKeys("satya.narayana.samal@ufinance.com");
+		     Thread.sleep(2000);
+		     driver.findElement(By.xpath(" //input[@type='submit']")).click();
+		     
+		     Thread.sleep(2000);
+		     
+		                                                                                                  
+		     driver.findElement(By.xpath("//input[@name='passwd']")).sendKeys("satya@123");
+		     Thread.sleep(2000);
+		     driver.findElement(By.xpath("//input[@type='submit']")).click();
+		     
+		     Thread.sleep(2000);
+		     
+		     driver.findElement(By.xpath("//input[@type='submit']")).click();
+		     
+		     Thread.sleep(2000);
 
 	}
 
